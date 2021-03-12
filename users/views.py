9b31c011dd45d70f4ff2eb5c4790e57379e25a26
waynecornwall <waynecornwall85@gmail.com>
2021-03-11 22:58:42 -0500

    added css styles and user authentication

--- users/views.py
+++ users/views.py
@@ -1,3 +1,23 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib import messages
 
 # Create your views here.
+
+
+def register(request):
+    if request.method != 'POST':
+        form = UserCreationForm()
+    else:
+        form = UserCreationForm(data=request.POST)
+        if form.is_valid():
+            new_user = form.save()
+            new_user.author = request.user
+            new_user.save()
+            messages.success(request, f'Hi {new_user.author.username.title()}, you can now log in to your account ')
+            return redirect('users:login')
+    context = {
+        'form' : form
+    }
+    return render(request, 'registration/register.html', context)
+
